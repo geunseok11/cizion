248a942250421ac33864be1a2fce1008f371bc7f
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-06-02 22:55:19 +0900

    Add context

--- project/component/Context.js
+++ project/component/Context.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const contextView = styled.view`
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+`;
 
 const Context = (props) => {
-  return <div>여기는 게시물 입니다</div>;
+  return (
+    <contextView>
+      <div>여기는 게시물 입니다</div>
+    </contextView>
+  );
 };
 
 export default Context;
