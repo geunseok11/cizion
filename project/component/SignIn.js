1c015308d6be0a7955d2778bc3350823f5be92bf
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-06-02 22:55:57 +0900

    Fix SignIn

--- project/component/SignIn.js
+++ project/component/SignIn.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import styled from "styled-components";
 
 const SignIn = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const text = {
+    email,
+    password,
+  };
+
+  const preloadValues = {
+    email: "email",
+    password: "password",
+  };
+
+  const onPressSignin = useCallback(() => {
+    props.navigation.navigate("Home");
+  }, [email, password]);
+
+  useEffect(() => {
+    register(email);
+    register(password);
+  }, [register]);
+
   return <div></div>;
 };
 
